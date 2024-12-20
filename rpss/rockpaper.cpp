23f169eb7665c0cd833c08002a981c67f6f1c91c
lios <lio@LAPTOP-E4123L2R.>
2024-12-20 08:45:07 -0600

    Created prompt for the user to select Rock Paper Scissors Lizard or Spock

--- rpss/rockpaper.cpp
+++ rpss/rockpaper.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
 int main(){
+	srand (time(NULL));
+	int cChoice = rand() % 3 + 1;
+	int uChoice;
 	//Prompt the user to select either Rock, Paper, Scissors, Lizard, or Spock
-
+	cout << "Select one of the following:\n1) Rock\n2) Paper\n3) Scissors\n4) Lizard\n5) Spock\nYour Choice:";
+	cin >> uChoice;
 	//Instruct the computer to randomly select the option
 
 	//Compare the choices to determine the winner 
