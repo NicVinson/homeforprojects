9da25845651533d0d9c9a57b2ca2fefefc580343
lios <lio@LAPTOP-E4123L2R.>
2024-12-20 08:35:35 -0600

    Set basic parameters

--- rpss/rockpaper.cpp
+++ rpss/rockpaper.cpp
@@ -1,1 +1,13 @@
 #include <iostream>
+
+using namespace std;
+
+int main(){
+	//Prompt the user to select either Rock, Paper, Scissors, Lizard, or Spock
+
+	//Instruct the computer to randomly select the option
+
+	//Compare the choices to determine the winner 
+
+	//Print out the winner
+}
