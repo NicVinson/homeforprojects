4360c0512fbd30a3923f61bf2755790f0bce36c9
lios <lio@LAPTOP-E4123L2R.>
2024-12-25 15:57:58 -0600

    Began Conditionals

--- rpss/rockpaper.cpp
+++ rpss/rockpaper.cpp
@@ -7,11 +7,20 @@ int main(){
 	srand (time(NULL));
 	int cChoice = rand() % 3 + 1;
 	int uChoice;
-	//Prompt the user to select either Rock, Paper, Scissors, Lizard, or Spock
-	cout << "Select one of the following:\n1) Rock\n2) Paper\n3) Scissors\n4) Lizard\n5) Spock\nYour Choice:";
-	cin >> uChoice;
-	//Instruct the computer to randomly select the option
 
+	cout << cChoice;
+	//Prompt the user to select either Rock, Paper, Scissors, Lizard, or Spock
+	cout << "Select one of the following:\n1) Rock\n2) Paper\n3) Scissors\nYour Choice:";
+	cin >> uChoice;
+
+	if(uChoice == cChoice){
+		cout << "It's a tie!";
+	}
+
+	switch(uChoice){
+		case 1:
+			cout << "User chose Rock";
+	}
 	//Compare the choices to determine the winner 
 
 	//Print out the winner
